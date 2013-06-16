17428cab4110f7db3b5adbd43c8eae1308fc0e8b
Branden Hall <bhall@automatastudios.com>
2013-06-16 16:41:42 -0400

    pulled into local var for speed in slitscan

--- blossom/patterns/slitscan.py
+++ blossom/patterns/slitscan.py
@@ -3,8 +3,6 @@ import settings
 from PIL import Image
 from twisted.python import log
 
-from profilehooks import timecall
-
 
 class SlitScan:
 
@@ -23,32 +21,30 @@ class SlitScan:
 
         self.data = [0, 0, 0] * settings.LEDS
 
-    @timecall(immediate=True)
     def draw(self):
         index = 0
         half_petal = settings.LEDS_PER_PETAL / 2
-        p2 = settings.PETALS * 2
-        right = self.offset * p2
-        left = right + 720
+        offset = self.offset
+        petals = settings.PETALS
+        image_size = self.image_size
+        data = self.data
+        raw_data = self.raw_data
 
-
-        for i in range(settings.PETALS):
-            i2 = i * 2
+        for i in range(petals):
 
             for j in range(half_petal):
-                jp2 = p2 * j
 
                 # left half of the petal
-                pixel = ((left - jp2 + i2) % self.image_size) * 3
-                self.data[index * 3 + 1] = self.raw_data[pixel]
-                self.data[index * 3 + 2] = self.raw_data[pixel + 1]
-                self.data[index * 3] = self.raw_data[pixel + 2]
+                pixel = (((offset * petals * 2) + 720 - (j * petals * 2) + (i * 2)) % image_size) * 3
+                data[index * 3 + 1] = raw_data[pixel]
+                data[index * 3 + 2] = raw_data[pixel + 1]
+                data[index * 3] = raw_data[pixel + 2]
 
                 # right half of the petal
-                pixel = ((right + jp2 + i2 + 1) % self.image_size) * 3
-                self.data[(index + half_petal) * 3 + 1] = self.raw_data[pixel]
-                self.data[(index + half_petal) * 3 + 2] = self.raw_data[pixel + 1]
-                self.data[(index + half_petal) * 3] = self.raw_data[pixel + 2]
+                pixel = (((offset * petals * 2) + (j * petals * 2) + (i * 2) + 1) % image_size) * 3
+                data[(index + half_petal) * 3 + 1] = raw_data[pixel]
+                data[(index + half_petal) * 3 + 2] = raw_data[pixel + 1]
+                data[(index + half_petal) * 3] = raw_data[pixel + 2]
 
                 index += 1
 
@@ -63,55 +59,3 @@ class SlitScan:
             self.offset -= self.height
 
         return self.data
-
-# import settings
-
-# from PIL import Image
-# from twisted.python import log
-
-# class SlitScan:
-
-#     def __init__(self, filename, speed=1):
-#         self.im = Image.open(filename)
-#         self.speed = speed
-#         self.offset = 0
-#         (self.width, self.height) = self.im.size
-#         self.raw_data = self.im.getdata()
-#         self.len_raw_data = len(self.raw_data)
-#         self.data = [0,0,0] * settings.LEDS
-
-#     def draw(self):
-#         index = 0
-#         half_petal = settings.LEDS_PER_PETAL / 2
-#         half_petals = settings.PETALS * 2
-
-#         for i in range(settings.PETALS):
-
-#             for j in range(half_petal):
-
-#                 # left half of the petal
-#                 pixel = ((self.offset * settings.PETALS * 2) + 720  - (j * settings.PETALS * 2) + (i * 2)) % self.len_raw_data
-#                 self.data[index * 3 + 1] = self.raw_data[pixel][0]
-#                 self.data[index * 3 + 2] = self.raw_data[pixel][1]
-#                 self.data[index * 3] = self.raw_data[pixel][2]
-
-#                 # right half of the petal
-#                 pixel = ((self.offset * settings.PETALS * 2) + (j * settings.PETALS * 2) + (i * 2) + 1) % self.len_raw_data
-#                 self.data[(index + half_petal) * 3 + 1] = self.raw_data[pixel][0]
-#                 self.data[(index + half_petal) * 3 + 2] = self.raw_data[pixel][1]
-#                 self.data[(index + half_petal) * 3] = self.raw_data[pixel][2]
-
-#                 index += 1
-
-#             index += half_petal
-
-#         self.offset += self.speed
-
-#         if self.offset < 0:
-#             self.offset = self.height + self.offset
-
-#         if self.offset >= self.height:
-#             self.offset -= self.height
-
-#         return self.data
-
